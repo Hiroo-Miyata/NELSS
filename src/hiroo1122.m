1fbe9eb7be86c5d762fa48aa75f6fe9cdcf906c0
Hiroo Miyata <hirorororo0329@gmail.com>
2022-12-02 14:02:33 -0500

    change data augmentation

--- src/hiroo1122.m
+++ src/hiroo1122.m
@@ -11,11 +11,11 @@ metadata = table2array(trial2HirooSongLabelEmotionLevel);
 tw = 10;
 ntrial = 10;
 
-labels = zeros(fs*tw*5, ntrial);
+eachlabels = zeros(fs*tw*5, ntrial);
 eegData = zeros(fs*tw*5, 29, ntrial);
 for i=(1:10)
     eegData(:, :, i) = bandpass(eeg(1+(i-1)*fs*tw*5:i*fs*tw*5, :),[4 45],fs);
-    labels(:, i) = emotion(1+(i-1)*fs*tw*5:i*fs*tw*5);
+    eachlabels(:, i) = emotion(1+(i-1)*fs*tw*5:i*fs*tw*5);
 end
 % for i=23:24
 %     eegData(:, :, i) = filteredeeg(1+(i-1)*fs*tw+10*fs:i*fs*tw+10*fs , :);
@@ -24,19 +24,29 @@ end
 %     end
 % end
 
-twsize = 224*4;
-featureData = zeros(ntrial, 3, twsize, twsize*5);
+%% feature extraction and data augmentation
+twsize = 10*fs;
+stride = 2*fs;
+imgsize = 224;
+nepoch = (size(eegData, 1) - twsize) / stride + 1;
+featureData = zeros(ntrial, nepoch, 3, imgsize, imgsize);
+labels = zeros(ntrial, nepoch);
 frontElectrode = [2,3,28];
 for tr=1:ntrial
     disp(tr)
-    for c=1:3
-        ch = frontElectrode(c);
-        [d,f,t] = wvd(eegData(:, ch,tr),fs, 'smoothedPseudo', NumFrequencyPoints=twsize,NumTimePoints=twsize*5);
-        featureData(tr, c, :, :) = d;
+        %% data augmentation
+    for i=1:nepoch
+        eachEEG = eegData((i-1)*stride+1:(i-1)*stride+twsize, :,tr);
+        for c=1:3
+            ch = frontElectrode(c);
+            [d,f,t] = wvd(eachEEG(:, ch),fs, 'smoothedPseudo', NumFrequencyPoints=imgsize,NumTimePoints=imgsize*2);
+            featureData(tr, i, c, :, :) = d(:, 1:2:end);
+            labels(tr, i) = round(mean(eachlabels((i-1)*stride+1:(i-1)*stride+twsize, tr)));
+        end
     end
 end
 data = featureData;
-save("50sResNet3.mat", "data", "labels")
+save("../data/preprocessed/50sResNet2.mat", "data", "labels")
 
 
 
--- src/hiroo1129.m
+++ src/hiroo1129.m
@@ -23,28 +23,41 @@ for i=23:24
     eachlabels(:, i) = emotion(1+(i-1)*fs*tw*6+2*fs*tw:i*fs*tw*6+fs*tw);
 end
 
-twsize = 224*4;
-featureData = zeros(ntrial, 3, twsize, twsize*5);
+
+%% feature extraction and data augmentation
+twsize = 10*fs;
+stride = 2*fs;
+imgsize = 224;
+nepoch = (size(eegData, 1) - twsize) / stride + 1;
+featureData = zeros(ntrial, nepoch, 3, imgsize, imgsize);
+labels = zeros(ntrial, nepoch);
 frontElectrode = [2,3,28];
 for tr=1:ntrial
     disp(tr)
-    for c=1:3
-        ch = frontElectrode(c);
-        [d,f,t] = wvd(eegData(:, ch,tr),fs, 'smoothedPseudo', NumFrequencyPoints=twsize,NumTimePoints=twsize*5);
-        featureData(tr, c, :, :) = d;
+        %% data augmentation
+    for i=1:nepoch
+        eachEEG = eegData((i-1)*stride+1:(i-1)*stride+twsize, :,tr);
+        for c=1:3
+            ch = frontElectrode(c);
+            [d,f,t] = wvd(eachEEG(:, ch),fs, 'smoothedPseudo', NumFrequencyPoints=imgsize,NumTimePoints=imgsize*2);
+            featureData(tr, i, c, :, :) = d(:, 1:2:end);
+            labels(tr, i) = round(mean(eachlabels((i-1)*stride+1:(i-1)*stride+twsize, tr)));
+        end
     end
 end
-for i=1:2
-    if i == 1
-        data = featureData(1:12,:,:,:);
-        labels = eachlabels(:, 1:12);
-    else
-        data = featureData(13:end,:,:,:);
-        labels = eachlabels(:, 13:end);
-    end
-    save("50sResNet"+num2str(i)+".mat", "data", "labels")
-end
-clear all
+data = featureData;
+% for i=1:2
+%     if i == 1
+%         data = featureData(1:12,:,:,:);
+%         labels = eachlabels(:, 1:12);
+%     else
+%         data = featureData(13:end,:,:,:);
+%         labels = eachlabels(:, 13:end);
+%     end
+%     save("50sResNet"+num2str(i)+".mat", "data", "labels")
+% end
+save("../data/preprocessed/50sResNet1.mat", "data", "labels")
+% clear all
 
 
 
